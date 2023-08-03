328c6a13b2bf93313e5a90ccc35c47c366c8f1e4
Sid Shingaki <sidalaytv@gmail.com>
2023-08-02 23:39:25 -0700

    add weapon mesh and setup weapon attachment

--- Source/Aura/Private/Character/Aura_BaseCharacter.cpp
+++ Source/Aura/Private/Character/Aura_BaseCharacter.cpp
@@ -7,6 +7,10 @@ AAura_BaseCharacter::AAura_BaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
 void AAura_BaseCharacter::BeginPlay()
