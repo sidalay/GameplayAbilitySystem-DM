ad472a53ea57e944edf1711436ff0491371c8f19
Sid Shingaki <sidalaytv@gmail.com>
2023-08-02 23:11:59 -0700

    add enemy class

---
+++ Source/Aura/Public/Character/Aura_Enemy.h
@@ -0,0 +1,17 @@
+// Copyright Yuki Shingaki
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Aura_BaseCharacter.h"
+#include "Aura_Enemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAura_Enemy : public AAura_BaseCharacter
+{
+	GENERATED_BODY()
+	
+};
