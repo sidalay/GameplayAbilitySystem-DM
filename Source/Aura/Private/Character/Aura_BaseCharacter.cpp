67360b9a4cbbd8179e58d9eaf0f955124b358424
Sid Shingaki <sidalaytv@gmail.com>
2023-08-02 21:47:01 -0700

    reorganized file hierarchy

---
+++ Source/Aura/Private/Character/Aura_BaseCharacter.cpp
@@ -0,0 +1,17 @@
+// Copyright Yuki Shingaki
+
+
+#include "Character/Aura_BaseCharacter.h"
+
+AAura_BaseCharacter::AAura_BaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAura_BaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Public/Character/Aura_BaseCharacter.h
@@ -0,0 +1,21 @@
+// Copyright Yuki Shingaki
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Aura_BaseCharacter.generated.h"
+
+UCLASS(Abstract)
+class AURA_API AAura_BaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAura_BaseCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+};
