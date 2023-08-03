60ffd84829fd0488348a2524ec32b01436c43dc5
Sid Shingaki <sidalaytv@gmail.com>
2023-08-02 23:12:13 -0700

    add character class

---
+++ Source/Aura/Private/Character/Aura_Character.cpp
@@ -0,0 +1,5 @@
+// Copyright Yuki Shingaki
+
+
+#include "Character/Aura_Character.h"
+
---
+++ Source/Aura/Public/Character/Aura_Character.h
@@ -0,0 +1,17 @@
+// Copyright Yuki Shingaki
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Aura_BaseCharacter.h"
+#include "Aura_Character.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAura_Character : public AAura_BaseCharacter
+{
+	GENERATED_BODY()
+	
+};
