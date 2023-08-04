0f226629f147602e09eeec38e57e7216c767f1fb
Sid Shingaki <sidalaytv@gmail.com>
2023-08-04 14:07:31 -0700

    create Aura_GameModeBase class

---
+++ Source/Aura/Private/Game/Aura_GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Yuki Shingaki
+
+
+#include "Game/Aura_GameModeBase.h"
+
---
+++ Source/Aura/Public/Game/Aura_GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Yuki Shingaki
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Aura_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAura_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
