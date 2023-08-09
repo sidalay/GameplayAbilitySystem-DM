f2c16c5c8706bd86efe240291cb938b2933877c9
Sid Shingaki <sidalaytv@gmail.com>
2023-08-09 14:43:31 -0700

    assign default values to AbilitySystemComponent and AttributeSet members in ctor

--- Source/Aura/Private/Character/Aura_Enemy.cpp
+++ Source/Aura/Private/Character/Aura_Enemy.cpp
@@ -3,6 +3,8 @@
 
 #include "Character/Aura_Enemy.h"
 
+#include "..\..\Public\AbilitySystem\Aura_AttributeSet.h"
+#include "AbilitySystem/Aura_AbilitySystemComponent.h"
 #include "Aura/Aura.h"
 
 AAura_Enemy::AAura_Enemy()
@@ -10,6 +12,11 @@ AAura_Enemy::AAura_Enemy()
 	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
 	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAura_AbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAura_AttributeSet>("AttributeSet");
 }
 
 void AAura_Enemy::HighlightActor()
