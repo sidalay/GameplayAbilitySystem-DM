974cfa43098e78f4f25f96ca707bff0e3e38aa70
Sid Shingaki <sidalaytv@gmail.com>
2023-08-09 14:26:21 -0700

    add aura abilitysystemcomponent class

---
+++ Source/Aura/Private/AbilitySystem/Aura_AbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Yuki Shingaki
+
+
+#include "AbilitySystem/Aura_AbilitySystemComponent.h"
+
---
+++ Source/Aura/Public/AbilitySystem/Aura_AbilitySystemComponent.h
@@ -0,0 +1,16 @@
+// Copyright Yuki Shingaki
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "Aura_AbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAura_AbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+};
