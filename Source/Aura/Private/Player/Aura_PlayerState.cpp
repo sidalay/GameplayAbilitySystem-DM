e5b7b34e0aa2356cc079e08266dbe3ea60f75c7f
Sid Shingaki <sidalaytv@gmail.com>
2023-08-09 14:26:47 -0700

    add aura playerstate class

---
+++ Source/Aura/Private/Player/Aura_PlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Yuki Shingaki
+
+
+#include "Player/Aura_PlayerState.h"
+
+AAura_PlayerState::AAura_PlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Aura/Public/Player/Aura_PlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Yuki Shingaki
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "Aura_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAura_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAura_PlayerState();
+};
