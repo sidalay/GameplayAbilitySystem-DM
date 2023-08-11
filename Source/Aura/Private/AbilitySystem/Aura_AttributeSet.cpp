e9d20da962ccbb059351adc541526a8970a2378f
Sid Shingaki <sidalaytv@gmail.com>
2023-08-11 14:47:25 -0700

    setup replication and register attributes into ability system

--- Source/Aura/Private/AbilitySystem/Aura_AttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/Aura_AttributeSet.cpp
@@ -2,4 +2,40 @@
 
 
 #include "..\..\Public\AbilitySystem\Aura_AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UAura_AttributeSet::UAura_AttributeSet()
+{
+
+}
+
+void UAura_AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAura_AttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAura_AttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAura_AttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAura_AttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAura_AttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAura_AttributeSet, Health, OldHealth);
+}
+
+void UAura_AttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAura_AttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAura_AttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAura_AttributeSet, Mana, OldMana);
+}
+
+void UAura_AttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAura_AttributeSet, MaxMana, OldMaxMana);
+}
